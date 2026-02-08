8b22cb9fd1551fea04d0452a035ac7a09c4be832
Ian <ijr011235@gmail.com>
2026-02-07 23:44:43 -0700

    started implementing tools

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,37 @@ use ollama_rs::generation::completion::request::GenerationRequest;
 //use tokio::io::{self, AsyncWriteExt};
 use std::io;
 
+fn read_file(path: String) -> Result<String, Box<dyn std::error::Error + Sync + Send>>{
+    let contents = std::fs::read_to_string(path).expect("Failed to read file");
+    return Ok(contents);
+}
+
+fn write_file(path: String, contents: String) -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
+    std::fs::write(path, contents).expect("Failed to write file");
+    return Ok(());
+}
+
+fn edit_file(path: String, old_content: String, new_content: String) -> Result<(), Box<dyn std::error::Error + Sync + Send>> {
+    return Ok(());
+}
+
+fn list_files() -> Result<Vec<String>, Box<dyn std::error::Error + Sync + Send>> {
+    let files = std::fs::read_dir(".").expect("Failed to read directory");
+    let mut file_names = Vec::new();
+    for file in files {
+        file_names.push(file.unwrap().path().display().to_string());
+    }
+    return Ok(file_names);
+}
+
+fn search_code(query: String) -> Result<Vec<String>, Box<dyn std::error::Error + Sync + Send>> {
+ return Ok(Vec::new());
+}
+
+fn executre_command() -> Result<Vec<String>, Box<dyn std::error::Error + Sync + Send>> {
+    return Ok(Vec::new());
+}
+
 async fn generate_response(prompt: &str) -> String {
     let ollama = Ollama::default();
 
